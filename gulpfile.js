b446a2d721600d0bfec07b716acaeea05f653094
iJame Thitiwut <thitiwut.to@peakengine.com>
2025-12-25 16:51:53 +0700

    Removes PEAK node integration
    Removes the PEAK node and related credentials, helpers, and assets.
    This change effectively removes the PEAK integration from the n8n project.

--- gulpfile.js
+++ gulpfile.js
@@ -3,30 +3,13 @@ const { task, src, dest } = require('gulp');
 
 task('build:icons', copyIcons);
 
-// function copyIcons() {
-// 	const nodeSource = path.resolve('nodes', '**', '*.{png,svg}');
-// 	const nodeDestination = path.resolve('dist', 'nodes');
-
-// 	src(nodeSource).pipe(dest(nodeDestination));
-
-// 	const credSource = path.resolve('credentials', '**', '*.{png,svg}');
-// 	const credDestination = path.resolve('dist', 'credentials');
-
-// 	return src(credSource).pipe(dest(credDestination));
-// }
-
 function copyIcons() {
-	// รวมทุก path ที่อยากให้ copy
 	const globs = [
 		'nodes/**/*.{png,svg}',
+		'nodes/**/*.{่json}',
 		'credentials/**/*.{png,svg}',
 		'assets/*.{png,svg}',
-		// ถ้ามีโฟลเดอร์ assets สำหรับ icon อื่น ๆ ให้ปลดคอมเมนต์บรรทัดนี้
-		// 'assets/**/*.{png,svg}',
 	];
-
-	// base: '.' = รักษาโครงโฟลเดอร์เดิม แล้วไปลงใต้ dist/
-	// เช่น nodes/PostReceipt/PEAK.png -> dist/nodes/PostReceipt/PEAK.png
 	return src(globs, { base: '.' }).pipe(dest('dist'));
 }
 
